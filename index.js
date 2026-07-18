bd48bc5df6c3661fcaebbfe81fb72c7d298d9805
Rudra Bandekar <rudrabandekar07@gmail.com>
2026-07-18 15:42:23 +0530

    Build CLI calculator

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log("Hello, Node.js!");
+const args = process.argv.slice(2);
+
+const operation = args[0];
+const a = Number(args[1]);
+const b = Number(args[2]);
+
+let result;
+
+switch (operation) {
+  case "add":
+    result = a + b;
+    break;
+
+  case "sub":
+    result = a - b;
+    break;
+
+  case "mul":
+    result = a * b;
+    break;
+
+  case "div":
+    result = a / b;
+    break;
+
+  default:
+    console.log("Usage:");
+    console.log("node index.js add 10 20");
+    process.exit();
+}
+
+console.log("Result =", result);
